21aafe7314a31aeaf3102d60509b44cba2bfe083
Davide <davide7antonino@icloud.com>
2025-11-20 15:30:31 +0100

    aggiunto esercizio 2

---
+++ Elementi_DOM/elementiDOM.js
@@ -0,0 +1,8 @@
+const p = document.getElementsByClassName("paragrafo");
+console.log(p);
+
+const thirdElement = document.getElementById("elemento-3");
+console.log(thirdElement);
+
+const h2 = document.querySelectorAll("h2");
+console.log(h2);
