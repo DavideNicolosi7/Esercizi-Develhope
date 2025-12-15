bdf519b6e3eadc44dd0ffd9f1b7f65851ed24822
Davide <davide7antonino@icloud.com>
2025-12-15 13:02:23 +0100

    Aggiunto esercizio Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import Hello from "./Hello";
-import { Message } from "./Message";
+import Welcome from "./Props/Welcome";
 
 export function App() {
   return (
     <div>
-      <Hello />
-      <Hello />{" "}
-      {/*Utilizzando più di una volta il componente Hello all'interno del componente App succede che viene renderizzato più volte il contenuto del componente Hello */}
-      <Message />{" "}
-      {/*Posso utilizzare il componente Message direttamente all'interno del componente App e utilizzandolo importa automaticamente il suo contenuto */}
+      <Welcome name="Davide" age={25} />
     </div>
   );
 }
-
-/*
-Suggerimento generale
-
-Per approfondire la tipizzazione di React con TypeScript: https://react-typescript-cheatsheet.netlify.app/
-Riepilogo miglioramenti suggeriti:
-
-Aggiungere sempre la tipizzazione di ritorno alle function component (: JSX.Element)
-Posizionare i commenti fuori dagli elementi JSX per migliorare la leggibilità.
-
-L'utilizzo ripetuto di <Hello /> dimostra che il componente viene renderizzato più volte come previsto. La spiegazione fornita nei commenti è appropriata,
-ma sarebbe consigliabile posizionare i commenti sopra la riga o 
-il blocco di codice a cui si riferiscono piuttosto che all'interno del markup JSX.
-In questo modo si migliora la leggibilità e si evita l'inclusione di "{" "}" superflui 
-o di commenti allineati a elementi inline
-*/
---
+++ src/Props/Welcome.tsx
@@ -0,0 +1,28 @@
+// In un'applicazione React, crea un componente funzionale Welcome che riceva una prop name
+// e visualizzi il messaggio "Welcome, {name}!" all'interno di un tag <p>.
+// Renderizza questo componente nel componente App, passando una prop name a tua scelta.
+// Imposta un valore predefinito per la prop name.
+// Aggiungi una seconda prop chiamata age e visualizzala sotto il messaggio di benvenuto,
+// all'interno di un tag <p> dopo il testo "Your age is ".
+// 4 .Modifica il valore passato alla prop name in modo che sia contenuto all'interno di un tag <strong>.
+// Estrai il messaggio "Your age is " in un nuovo componente chiamato Age
+// e renderizzalo all'interno del componente Welcome.
+// Passa al componente Age la prop age che Welcome sta ricevendo dal componente App.
+
+import Age from "./Age";
+
+export type WelcomeProps = {
+  name?: string;
+  age: number;
+};
+
+export default function Welcome({ name = "User", age }: WelcomeProps) {
+  return (
+    <div>
+      <p>
+        <strong>Welcome, {name}!</strong>
+      </p>
+      <Age age={age} />
+    </div>
+  );
+}
